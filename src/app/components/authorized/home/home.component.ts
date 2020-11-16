46b97e1379712886200b53ba9a8a5212e2423041
Vijay <nvijay.blr@gmail.com>
2020-11-16 14:52:35 +0530

    Community page UI

--- src/app/components/authorized/home/home.component.ts
+++ src/app/components/authorized/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from '../../../services/http-service.service';
 import { AuthGuardService } from '../../../services/auth-guard.service';
+import { MessageService } from '../../../services/message.service';
+import { appConfig } from '../../../app.config';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +13,27 @@ import { AuthGuardService } from '../../../services/auth-guard.service';
 export class HomeComponent implements OnInit {
 
   user: any = {};
-  constructor(private authGuardService: AuthGuardService, ) {
-    this.user = this.authGuardService.getLoggedInUserDetails();
-  }
+  professional: any = {};
+  userId: any = '';
+  isUserLoggedIn = false;
+  isEditMode = false;
+  appConfig: any = {};
+  isCurrentUser = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authGuardService: AuthGuardService,
+    private messageService: MessageService) {
+      this.appConfig = appConfig;
+    }
 
   ngOnInit() {
+    this.isUserLoggedIn = this.authGuardService.isUserLoggedIn();
+    this.user = this.authGuardService.getLoggedInUserDetails();
+    this.userId = this.user.id;
   }
 
 }
